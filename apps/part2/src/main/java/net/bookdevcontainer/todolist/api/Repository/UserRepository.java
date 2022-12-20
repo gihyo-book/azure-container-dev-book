db17e7ffa41d92d4ef760fe33cb230552858df7c
hyatsu <hyatsu@microsoft.com>
2022-12-20 21:00:25 +0900

    mysql connection with user and password

---
+++ apps/part2/src/main/java/net/bookdevcontainer/todolist/api/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package net.bookdevcontainer.todolist.api.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
