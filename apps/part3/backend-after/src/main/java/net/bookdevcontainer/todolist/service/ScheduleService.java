bcc10f95f860e5be8d9f0b90c373539091bd26dd
Shiho ASA <asashiho@hotmail.co.jp>
2022-12-27 10:55:12 +0900

    Delete: unnecessary public / thanks @Logico_jp

--- apps/part3/backend-after/src/main/java/net/bookdevcontainer/todolist/service/ScheduleService.java
+++ apps/part3/backend-after/src/main/java/net/bookdevcontainer/todolist/service/ScheduleService.java
@@ -2,8 +2,7 @@ package net.bookdevcontainer.todolist.service;
 
 import org.springframework.http.ResponseEntity;
 
-
 public interface ScheduleService {
-    public ResponseEntity<String> schedule();
+    ResponseEntity<String> schedule();
 
 }
