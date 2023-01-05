110087535032807e2d94ab0d4355d2f828f9eb7d
hyatsu <hyatsu@microsoft.com>
2023-01-06 00:54:40 +0900

    comment out for running test code

--- apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/UserRepository.java
+++ apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package net.bookdevcontainer.todolist.api.repository;
+// package net.bookdevcontainer.todolist.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+// // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+// public interface UserRepository extends CrudRepository<User, Integer> {
 
-}
+// }
