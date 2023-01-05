ef7375fd56c0da891f74c1dffdd240f2eef69396
hyatsu <hyatsu@microsoft.com>
2023-01-06 02:00:45 +0900

    change class name of JPA to Task from User

--- apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/TaskRepository.java
+++ apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/TaskRepository.java
@@ -5,6 +5,6 @@
 // // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // // CRUD refers Create, Read, Update, Delete
 
-// public interface UserRepository extends CrudRepository<User, Integer> {
+// public interface TaskRepository extends CrudRepository<Task, Integer> {
 
 // }
